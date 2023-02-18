d4cde32005c46db20b924c6ce7285cfc7e4fa50f
Cal122 <callumwork1122@gmail.com>
2023-02-18 20:16:05 +0000

    Added input validation for password length

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ import random
 
 def generate_password():
 
-    characters = list(string.ascii_letters + string.digits) 
+    characters = list(string.ascii_letters + string.digits + "!@#$%^&*()") 
     random.shuffle(characters)
     password = []
 
     answer_input = str(input("Would you like to generate a password? (Y/N): ").lower())
 
     if answer_input == "y":
+        
         pswrd_len_input = int(input("How long should the password be?: "))
+        while pswrd_len_input <= 0:
+            pswrd_len_input = int(input("How long should the password be?: "))
 
         for n in range(pswrd_len_input):
             password.append(random.choice(characters))
